61bd78ebabc0072bb177dd83b8bd8a0a44e7bde0
Ömer Can Korkmaz <omercankorkmaz0@gmail.com>
2020-11-05 09:49:22 +0300

    INITIAL

---
+++ src/main/java/com/bookclient/Microservices3BookClientApplication.java
@@ -0,0 +1,13 @@
+package com.bookclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices3BookClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices3BookClientApplication.class, args);
+	}
+
+}
